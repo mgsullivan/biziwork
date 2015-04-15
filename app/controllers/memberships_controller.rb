898c0e4ada7388b2f5aa72118f69b338e062146b
Matt on Elementary VM <mgsullivan.musc@gmail.com>
2015-04-15 11:17:06 -0400

    finished rebrand

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,5 +1,8 @@
 class MembershipsController < ApplicationController
-	before_action :set_membership, only: [ :destroy, :create]
+	before_action :set_membership, only: [ :destroy]
+	before_action :set_team, only: [:create]
+	before_action :set_user, only: [:create]
+	respond_to :html
 	def destroy
 		@membership.destroy
 		respond_to do |format|
@@ -19,22 +22,38 @@ class MembershipsController < ApplicationController
 	end
 	def new
     	@membership = Membership.new
-    	respond_with(@membership)
+    	
   	end
   	def create
+<<<<<<< HEAD
   		session[:return_to] ||= request.referer
   		unless Membership.where(team: membership.team, user: membership.user).size >0
   			membership.save
+=======
+  		@membership = Membership.new(user:@user,team:@team)
+  		unless Membership.where(team: @team, user: @user).size >0
+
+  			@membership.save
+>>>>>>> 0e470ac967b6455507019380225358a1af8e380a
   		end
+  		respond_to do |format|
+      		format.html {redirect_to admin_path, notice: "Request submitted"}
+    	end
 
   	end
 
 private
+	def set_team
+		@team = Team.find(params[:team])
+	end 
+	def set_user
+		@user = User.find(params[:user])
+	end
 	def set_membership
 		@membership = Membership.find(params[:id])
 	end
 	def membership_params
-      params.require(:membership).permit(:id, :team_id, :user_id)
+      params.require().permit(:id, :team_id, :user_id)
     end
 
 end
